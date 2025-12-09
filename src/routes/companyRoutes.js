b7260d6987b24e03aa26a4ed7c3c2616bc3a7bd7
avani-fmc <Software2@FathhomAI.onmicrosoft.com>
2025-12-09 09:56:50 +0530

    create new feature company - to add, edit, update, delete and get company and by companyid id-controller-routes

---
+++ src/routes/companyRoutes.js
@@ -0,0 +1,17 @@
+// src/routes/companyRoutes.js
+import { Router } from 'express';
+import {
+  getAllCompanies,
+  getCompanyById,
+  createCompany,
+  updateCompany,
+  deleteCompany,
+} from '../controller/companyController.js';
+
+export const router = Router();
+
+router.get('/', getAllCompanies);
+router.get('/:id', getCompanyById);
+router.post('/', createCompany);
+router.put('/:id', updateCompany);
+router.delete('/:id', deleteCompany);
