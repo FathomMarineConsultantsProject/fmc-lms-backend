73a4a781dabf749953596ce7e4f572a7a76d55f5
avani-fmc <Software2@FathhomAI.onmicrosoft.com>
2025-12-15 11:12:08 +0530

    feat/incident-setup controller routes for incident report

---
+++ src/routes/incidentRoutes.js
@@ -0,0 +1,17 @@
+// // src/routes/incidentRoutes.js
+// import { Router } from 'express';
+// import {
+//   getAllIncidents,
+//   getIncidentById,
+//   createIncident,
+//   updateIncident,
+//   deleteIncident,
+// } from '../controller/incidentsController.js';
+
+// export const router = Router();
+
+// router.get('/', getAllIncidents);
+// router.get('/:id', getIncidentById);
+// router.post('/', createIncident);
+// router.put('/:id', updateIncident);
+// router.delete('/:id', deleteIncident);
