9b5160a8defd3080d58a0e5b4262f190817abf58
FathomMarineConsultantsProject <Project@FathhomAI.onmicrosoft.com>
2026-02-02 10:15:15 +0530

    branch/vercel-activityGreen-activity log button green every required seconds1

--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -1,7 +1,7 @@
 // src/routes/activityRoutes.js
 import { Router } from 'express';
 import { requireAuth } from '../middleware/requireAuth.js';
-import { trackActivity, getActivityLogs } from '../controller/activityLogsController.js';
+import { trackActivity, getActivityLogs, getRecentActivityStatus, } from '../controller/activityLogsController.js';
 
 export const router = Router();
 
