5127e71ada388ce34501a750ef1d1b7635a01cb3
shubham452 <shubham.sk70@gmail.com>
2026-09-02 16:18:34 +0530

    immersive4

--- src/routes/immersiveRoutes.js
+++ src/routes/immersiveRoutes.js
@@ -9,7 +9,7 @@ import {
     updateEquipmentStatus, 
     updateEquipmentDetails 
 } from '../controller/immersiveController.js';
-
+import { requireAuth } from "../middleware/requireAuth.js";
 import { allowRoles } from '../middleware/rbac.js';
 export const router = express.Router();
 // ==========================================
@@ -17,21 +17,21 @@ export const router = express.Router();
 // ==========================================
 
 // Public / All Authenticated Users (Assuming your main auth middleware is applied in server.js)
-router.get('/scenarios', getAllScenarios);
+router.get('/scenarios',requireAuth, getAllScenarios);
 
 // Protected (SuperAdmin: 1 & Admin: 2 only)
-router.post('/scenarios', allowRoles(1, 2), createScenario);
-router.patch('/scenarios/:id', allowRoles(1, 2), updateScenario);
+router.post('/scenarios',requireAuth, allowRoles(1, 2), createScenario);
+router.patch('/scenarios/:id',requireAuth, allowRoles(1, 2), updateScenario);
 
 // ==========================================
 // EQUIPMENT ROUTES
 // ==========================================
 
 // Public / All Authenticated Users
-router.get('/equipment',getAllEquipment);
+router.get('/equipment',requireAuth,getAllEquipment);
 
 // Protected (SuperAdmin: 1 & Admin: 2 only)
-router.post('/equipment', allowRoles(1, 2), createEquipment);
-router.patch('/equipment/:id/status', allowRoles(1, 2), updateEquipmentStatus);
-router.patch('/equipment/:id', allowRoles(1, 2), updateEquipmentDetails);
+router.post('/equipment',requireAuth, allowRoles(1, 2), createEquipment);
+router.patch('/equipment/:id/status',requireAuth, allowRoles(1, 2), updateEquipmentStatus);
+router.patch('/equipment/:id',requireAuth, allowRoles(1, 2), updateEquipmentDetails);
 
