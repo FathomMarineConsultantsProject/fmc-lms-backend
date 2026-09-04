f5cd5b2c5be92eae89ecac684a2d34ea942e9f01
shubham452 <shubham.sk70@gmail.com>
2026-09-04 15:12:37 +0530

    ai-des

--- src/routes/aiRoutes.js
+++ src/routes/aiRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/requireAuth.js";
 import { handleChatBotQuery } from "../controller/aiController.js"
-import { generateCourseDescription } from "../controller/aiController.js";
+import { generateCourseDescription, generateTableOfContents } from "../controller/aiController.js";
 
 const router = Router();
 
