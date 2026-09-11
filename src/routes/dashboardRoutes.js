4dc8e9b885e25338e7569a2e63e46ac0d2ff37f7
shubham452 <shubham.sk70@gmail.com>
2026-09-11 18:26:02 +0530

    dsh2

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -5,7 +5,7 @@ import {
   getTrainingDashboard,
   getShipTrainingDashboard,
   getSeafarerTrainingDashboard,
-} from "../controllers/dashboardController.js";
+} from "../controller/dashboardController"
 
 import { requireAuth } from "../middleware/requireAuth.js";
 import { allowRoles } from "../middleware/rbac.js";
